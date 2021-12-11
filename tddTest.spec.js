b9c8d8dda99a00002e400fa1955606ed78235552
UNIcode <82393165+UNI-Meang@users.noreply.github.com>
2021-12-11 22:05:40 +0900

    TDD_tset_using jasmine

---
+++ tddTest.spec.js
@@ -0,0 +1,9 @@
+// test unit 모음
+describe('자스민 테스트 입니다', () => {
+    //  test unit
+    it('덧셈을 하는 함수입니다.', () => {
+        let num = 10;
+
+        expect(plusOne(num)).toBe(num + 1);
+    })
+})
